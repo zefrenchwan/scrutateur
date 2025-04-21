2dc437d21a6a3c3f23d790325f097348d52c4dff
cenalon@yahoo.fr <cenalon@yahoo.fr>
2025-04-21 08:55:02 +0200

    validate gorm (worst code, but works)

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/zefrenchwan/scrutateur.git/storage"
 )
 
 func main() {
 	engine := gin.New()
 	engine.GET("/test", func(context *gin.Context) {
+		if db, err := storage.CreateDatabase("postgres", "popo"); err != nil {
+			fmt.Println(err)
+		} else {
+			var counter int64
+			db.Table("auth.users").Count(&counter)
+			fmt.Println(counter)
+		}
 		context.JSON(http.StatusAccepted, "Alors, euh... Bonchouriiin")
 	})
 	engine.Run(":3000")
