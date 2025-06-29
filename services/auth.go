96462177bb650fca7f53a6272ebb6e8b739d4457
cenalon@yahoo.fr <cenalon@yahoo.fr>
2025-06-29 11:44:07 +0200

    Small changes, just comments and updates

--- services/auth.go
+++ services/auth.go
@@ -6,6 +6,8 @@ import (
 	"github.com/zefrenchwan/scrutateur.git/dto"
 )
 
+// HasMinimumAccessAuth tests if user has sufficient auth to meet expected roles expectations.
+// Formally, it means getting the union of roles and localRoles, and then find if there is one role that is also in expectedRoles
 func HasMinimumAccessAuth(roles []dto.GrantRole, localRoles []dto.GrantRole, expectedRoles []dto.GrantRole) bool {
 	result := append(roles, localRoles...)
 	result = slices.Compact(result)
